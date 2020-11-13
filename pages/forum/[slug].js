232eab39a39b7139ae4cc62172fa299d52d12474
fhs42724 <fhs42724@fh-salzburg.ac.at>
2020-11-13 21:52:51 +0100

    SSR test

--- pages/forum/[slug].js
+++ pages/forum/[slug].js
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import {useRouter}from"next/router";
 import Error from "next/error";
 
 //Welche Pfade prerendered werden können
-export const getStaticPaths=async ()=>{
-    return {
-        paths: [
-         { params: { slug: 'neuigkeiten' } }
-        ],
-        fallback: true, 
-      };
-};
+// export const getStaticPaths=async ()=>{
+//     return {
+//         paths: [
+//          { params: { slug: 'neuigkeiten' } }
+//         ],
+//         fallback: true, 
+//       };
+// };
 
-export const getStaticProps = async ({params}) => {
-  const res = await fetch("https://iou-andreas.herokuapp.com/api/v1/users");
+// export const getStaticProps = async ({params}) => {
+//   const res = await fetch("https://iou-andreas.herokuapp.com/api/v1/users");
+//   const userData = await res.json();
+//   const users = userData.data;
+//   const slug=params.slug;
+//   return {
+//     props: {
+//       users,
+//       slug
+//     },
+//     revalidate: 1,
+//   };
+// };
+export async function getServerSideProps(context) {
+      const res = await fetch("https://iou-andreas.herokuapp.com/api/v1/users");
   const userData = await res.json();
   const users = userData.data;
-  const slug=params.slug;
-  return {
-    props: {
-      users,
-      slug
-    },
-    revalidate: 1,
-  };
-};
+  const slug=context.params.slug;
+    return {
+      props: {users,slug}, // will be passed to the page component as props
+    }
+  }
+  
 
 
 function Subforum({ users, slug }) {
-    const {isFallback} = useRouter();
 
+    const {isFallback} = useRouter();
     if(isFallback){
         return <Error statusCode={404} title="URL could not be found" />
     } 
