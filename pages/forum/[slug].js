b06cf807b074cc0daf4849d94a0062a23bfd0109
fhs42724 <fhs42724@fh-salzburg.ac.at>
2020-11-13 16:58:42 +0100

    SSR test

---
+++ pages/forum/[slug].js
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+
+//Welche Pfade prerendered werden können
+export const getStaticPaths=async ()=>{
+    return {
+        paths: [
+         { params: { slug: 'neuigkeiten' } }
+        ],
+        fallback: true, 
+      };
+};
+
+export const getStaticProps = async ({params}) => {
+  const res = await fetch("https://iou-andreas.herokuapp.com/api/v1/users");
+  const userData = await res.json();
+  const users = userData.data;
+  const slug=params.slug;
+  return {
+    props: {
+      users,
+      slug
+    },
+    revalidate: 1,
+  };
+};
+
+
+function Subforum({ users, slug }) {
+  return (
+    <>
+        <ul>
+            {users.map(user=>{
+                return <li key={user.id}><Link href={`${slug}/${user.id}`}>{user.attributes.name}</Link></li>
+            })}
+        </ul>
+    </>
+  );
+}
+
+export default Subforum;
---
+++ pages/forum/[slug]/[id].js
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "next/link";
+
+//Welche Pfade prerendered werden können
+export const getStaticPaths=async ()=>{
+
+    const res = await fetch("https://iou-andreas.herokuapp.com/api/v1/users");
+    const userData = await res.json();
+    const users = userData.data;
+    const paths= users.map(user=>{
+        return{
+         params: {slug: 'neuigkeiten', id: user.id} }
+    });
+
+    return {
+        paths,
+        fallback: true, 
+      };
+};
+
+export const getStaticProps = async ({params}) => {
+  const res = await fetch(`https://iou-andreas.herokuapp.com/api/v1/users/${params.id}`);
+  const userData = await res.json();
+  const user = userData.data;
+  return {
+    props: {
+      user,
+    },
+    revalidate: 1,
+  };
+};
+
+function UserView({ user }) {
+    return (
+      <>
+         <h1>{user.attributes.name}</h1>
+         <h2>{user.attributes.email}</h2>
+      </>
+    );
+  }
+  
+  export default UserView;
