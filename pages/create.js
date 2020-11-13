3698c35c5718ac95e0b9b6f2477e263d00a86189
fhs42724 <fhs42724@fh-salzburg.ac.at>
2020-11-13 21:37:47 +0100

    SSR test

---
+++ pages/create.js
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from "next/link"
+import { useState } from 'react'
+import {useRouter} from "next/router";
+
+
+
+
+export default function Create() {
+
+    const [username, setUsername]=useState("");
+    const [email, setEmail]=useState("");
+    const [password, setPassword]=useState("");
+    const router=useRouter();
+
+    const submit= async ()=>{
+        let data={data:{
+            type: "user",
+         attributes: {
+        name: username,
+        email: email,
+        password: password,
+        },},};
+        try{
+        const response = await fetch("https://iou-andreas.herokuapp.com/api/v1/users", {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+          });
+          let result=response.json();
+          console.log(result);
+          router.push("/forum/neuigkeiten")
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+  return (
+    <>
+        <input onChange={(e)=>setUsername(e.target.value)}type="text" name="username" value={username}></input>
+        <input onChange={(e)=>setEmail(e.target.value)}type="email" name="email" value={email}></input>
+        <input onChange={(e)=>setPassword(e.target.value)} type="password" name="password" value={password}></input>
+        <button onClick={()=>submit()}>Absenden</button>
+
+    </>
+  )
+}
